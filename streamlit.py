673a46cb4e94a2c4b5c26c8cc6aeef02de0b35ad
yemaney <yemane_@outlook.com>
2021-06-02 23:43:31 -0400

    fixing streamlit

--- streamlit.py
+++ streamlit.py
@@ -21,11 +21,10 @@ def get_images(link):
     link = poster.get('src')
     return  link
 
-df = pd.read_csv('movie_meta.csv')
+df = pd.read_csv('streamlit/movie_meta.csv')
     
 type = st.sidebar.selectbox('Type',
                             ('Home', 'Content Based','Collabrative'))
-
 
 if type == 'Home':
     st.title('Movie Recommender')
@@ -68,15 +67,11 @@ if type == 'Content Based':
 
     df
     
-    cosine_sim = np.load('cosine_sim.npy')
+    cosine_sim = np.load('streamlit/cosine_sim.npy')
     
     @st.cache
     def get_movie_idx(title, df=df):
         return df[df['title'] == title].index[0]
-
-    @st.cache
-    def new_func(score_series):
-        return score_series.iloc[1:7]
 
     @st.cache
     def get_top_3(title, df=df):
@@ -90,12 +85,11 @@ if type == 'Content Based':
         cont1 = df.loc[top_10_indices[0], 'title']
         cont2 = df.loc[top_10_indices[1], 'title']
         cont3 = df.loc[top_10_indices[2], 'title']
-        cont4 = df.loc[top_10_indices[3], 'title']
-        cont5 = df.loc[top_10_indices[4], 'title']
-        cont6 = df.loc[top_10_indices[5], 'title']
-                
-        return cont1, cont2, cont3, cont4, cont5, cont6
+        
+        return cont1, cont2, cont3
 
+    def new_func(score_series):
+        return score_series.iloc[1:4]
  
     st.markdown('---')
    
@@ -104,11 +98,11 @@ if type == 'Content Based':
     title = st.selectbox('Movie title', df.title.values)
     
     if title is not None:
-        a, b, c, d, e, f = get_top_3(title)
+        a, b, c = get_top_3(title)
         st.markdown('## Here are your recommendations')
         
         links = []
-        for i in [a, b, c, d, e, f]:
+        for i in [a,b,c]:
             link = create_link(i)
             links.append(link)
                 
@@ -116,24 +110,17 @@ if type == 'Content Based':
         for i in links:
             image = get_images(i)
             images.append(image)        
-       
-        
+
         col1, col2, col3 = st.beta_columns(3)
         with col1:
             st.image(images[0])
             st.markdown(f'[{a}]({links[0]})')
-            st.image(images[3])
-            st.markdown(f'[{d}]({links[3]})')
         with col2:
             st.image(images[1])
             st.markdown(f'[{b}]({links[1]})')
-            st.image(images[4])
-            st.markdown(f'[{e}]({links[4]})')
         with col3:
             st.image(images[2])
             st.markdown(f'[{c}]({links[2]})')
-            st.image(images[5])
-            st.markdown(f'[{f}]({links[5]})')
 
 if type == 'Collabrative':
     st.title('Collabrative Filtering')
@@ -155,24 +142,21 @@ if type == 'Collabrative':
     
     df 
     
-    corr_mat = np.load('item_colab.npy')
-    ratings = pd.read_csv('ratings.csv')
+    corr_mat = np.load('streamlit/item_colab_corr.npy')
+    ratings = pd.read_csv('streamlit/ratings.csv')
     
     @st.cache
     def item_colab(title, ratings=ratings, corr_mat=corr_mat):
         col_idx = ratings.columns.get_loc(title)
 
         corr_specific = corr_mat[col_idx]
-        item1,item2,item3, item4,item5,item6 = np.argsort(corr_specific)[::-1][1:7]
+        item1,item2,item3 = np.argsort(corr_specific)[::-1][1:4]
         
         item1 = ratings.columns[item1]
         item2 = ratings.columns[item2]
         item3 = ratings.columns[item3]
-        item4 = ratings.columns[item4]
-        item5 = ratings.columns[item5]
-        item6 = ratings.columns[item6]
         
-        return item1, item2, item3, item4, item5, item6
+        return item1, item2, item3
     
     st.markdown('---')
 
@@ -183,11 +167,11 @@ if type == 'Collabrative':
     
     
     if title is not None:
-        a, b, c, d, e, f = item_colab(title)
+        a, b, c = item_colab(title)
         st.markdown('## Here are your recommendations')
         
         links = []
-        for i in [a, b, c, d, e, f]:
+        for i in [a,b,c]:
             link = create_link(i)
             links.append(link)
                 
@@ -195,23 +179,16 @@ if type == 'Collabrative':
         for i in links:
             image = get_images(i)
             images.append(image)        
-       
-        
+
         col1, col2, col3 = st.beta_columns(3)
         with col1:
             st.image(images[0])
             st.markdown(f'[{a}]({links[0]})')
-            st.image(images[3])
-            st.markdown(f'[{d}]({links[3]})')
         with col2:
             st.image(images[1])
             st.markdown(f'[{b}]({links[1]})')
-            st.image(images[4])
-            st.markdown(f'[{e}]({links[4]})')
         with col3:
             st.image(images[2])
             st.markdown(f'[{c}]({links[2]})')
-            st.image(images[5])
-            st.markdown(f'[{f}]({links[5]})')
     
     st.markdown('---')
